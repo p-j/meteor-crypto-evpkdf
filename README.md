b4bccb54443443513faa99336c8b1ed646a5f6f5
Jeremie Parker <jparker@guimiesoft.com>
2014-09-10 10:38:34 +0200

    changed version to 0.1.0 - updated readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-TODO: write something meaningful here
+Ported to Meteor from [CryptoJS](https://code.google.com/p/crypto-js/). 
+
+Provide a key derivation function that is meant to conform with [EVP_BytesToKey](http://www.openssl.org/docs/crypto/EVP_BytesToKey.html).
+
+Related package:
+
+- [`jparker:crypto-aes`](https://github.com/p-j/meteor-crypto-aes)
+
+Other CryptoJS package : 
+
+- [`jparker:crypto-md5`](https://github.com/p-j/meteor-crypto-md5)
+- [`jparker:crypto-sha1`](https://github.com/p-j/meteor-crypto-sha1)
+- [`jparker:crypto-sha256`](https://github.com/p-j/meteor-crypto-sha256)
+- [`jparker:crypto-base64`](https://github.com/p-j/meteor-crypto-base64)
+- [`jparker:crypto-hmac`](https://github.com/p-j/meteor-crypto-hmac)
+
+See also
+--------
+The CryptoJS project lives at <https://code.google.com/p/crypto-js/>.
--- package.js
+++ package.js
@@ -1,30 +1,31 @@
 Package.describe({
   summary: 'EvpKDF Package for CryptoJS, standard secure algorithms',
-  version: '3.1.2',
+  version: '0.1.0',
   git: 'https://github.com/p-j/meteor-crypto-EvpKDF.git'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.1.1');
+
   api.use([
-    'jparker:crypto-core@3.1.2',
-    'jparker:crypto-md5@3.1.2'
+    'jparker:crypto-core@0.1.0',
+    'jparker:crypto-md5@0.1.0'
   ], ['client', 'server']);
 
-	api.imply([
+  api.imply([
     'jparker:crypto-core',
     'jparker:crypto-md5'
   ], ['client', 'server']);
 
-  api.addFiles(['lib/evpkdf.js']);
+  api.addFiles('lib/evpkdf.js', ['client', 'server']);
 });
 
 Package.onTest(function (api) {
-	api.use([
-		'jparker:crypto-core@3.1.2',
-		'jparker:crypto-md5@3.1.2',
-		'tinytest'
-	], ['client', 'server']);
+  api.use([
+    'jparker:crypto-core@0.1.0',
+    'jparker:crypto-md5@0.1.0',
+    'tinytest'
+  ], ['client', 'server']);
 
-	api.addFiles('tests/tests.js', ['client', 'server']);
+  api.addFiles('tests/tests.js', ['client', 'server']);
 });
