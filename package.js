88a75f26c45b5cf76fbaece115b12789ef76b5dd
Jeremie Parker <jparker@guimiesoft.com>
2014-11-27 20:53:10 +0100

    It's always better to load the library you want to test...

--- package.js
+++ package.js
@@ -1,31 +1,32 @@
 Package.describe({
-  summary: 'EvpKDF Package for CryptoJS, standard secure algorithms',
-  version: '0.1.0',
-  git: 'https://github.com/p-j/meteor-crypto-EvpKDF.git'
+	summary: 'EvpKDF Package for CryptoJS, standard secure algorithms',
+	version: '0.1.0',
+	git: 'https://github.com/p-j/meteor-crypto-EvpKDF.git'
 });
 
-Package.onUse(function (api) {
-  api.versionsFrom('METEOR@0.9.1.1');
+Package.onUse(function(api) {
+	api.versionsFrom('METEOR@0.9.1.1');
 
-  api.use([
-    'jparker:crypto-core@0.1.0',
-    'jparker:crypto-md5@0.1.0'
-  ], ['client', 'server']);
+	api.use([
+		'jparker:crypto-core@0.1.0',
+		'jparker:crypto-md5@0.1.0'
+	], ['client', 'server']);
 
-  api.imply([
-    'jparker:crypto-core',
-    'jparker:crypto-md5'
-  ], ['client', 'server']);
+	api.imply([
+		'jparker:crypto-core',
+		'jparker:crypto-md5'
+	], ['client', 'server']);
 
-  api.addFiles('lib/evpkdf.js', ['client', 'server']);
+	api.addFiles('lib/evpkdf.js', ['client', 'server']);
 });
 
-Package.onTest(function (api) {
-  api.use([
-    'jparker:crypto-core@0.1.0',
-    'jparker:crypto-md5@0.1.0',
-    'tinytest'
-  ], ['client', 'server']);
+Package.onTest(function(api) {
+	api.use([
+		'jparker:crypto-core@0.1.0',
+		'jparker:crypto-md5@0.1.0',
+		'jparker:crypto-evpkdf@0.1.0',
+		'tinytest'
+	], ['client', 'server']);
 
-  api.addFiles('tests/tests.js', ['client', 'server']);
+	api.addFiles('tests/tests.js', ['client', 'server']);
 });
